f3d55b885d8efaf87c275b4754fcec7b76a5c8e0
qhama991207 <mdekaziq@gmail.com>
2024-10-09 09:28:29 +0100

    studentmanagement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { TableComponent } from './table/table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StudentComponent, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
